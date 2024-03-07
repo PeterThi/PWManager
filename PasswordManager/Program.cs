a65a7dbe99f3149b0a1a0fb1fc13e04951a1e9c9
PeterThi <pete9817@gmail.com>
2024-03-07 15:10:48 +0100

    Init project

---
+++ PasswordManager/Program.cs
@@ -0,0 +1,16 @@
+﻿bool isLoggedIn = false;
+
+while (!isLoggedIn)
+{
+    Console.WriteLine("Insert Credentials, please ");
+    Console.Write("Username: ");
+    string userNameFromInput = Console.ReadLine();
+    Console.Write("Password: ");
+    string passwordFromInput = Console.ReadLine();
+    if (passwordFromInput != null)
+    {
+        isLoggedIn = true;
+    }
+
+
+}
